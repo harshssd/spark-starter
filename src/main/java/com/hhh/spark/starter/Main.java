392d0531e047bfe56d791fad53655a9bb16ea8b9
Hassan Somashekara <somash@amazon.com>
2016-07-02 19:27:58 -0700

    Organize the Handlebar files to Blocks and Partials.

--- src/main/java/com/hhh/spark/starter/Main.java
+++ src/main/java/com/hhh/spark/starter/Main.java
@@ -3,11 +3,14 @@ package com.hhh.spark.starter;
 import com.hhh.spark.starter.model.SimpleTaskDAO;
 import com.hhh.spark.starter.model.TaskDAO;
 import spark.ModelAndView;
-import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
 
 /**
  * @Author Harsha
@@ -18,15 +21,17 @@ public class Main
 {
     public static void main(String[] args)
     {
+        staticFileLocation("/public");
+
         TaskDAO taskDAO = new SimpleTaskDAO();
 
-        Spark.get("/", (request, response) -> {
+        get("/", (request, response) -> {
             Map<String, String> model = new HashMap<String, String>();
             model.put("username", request.cookie("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        Spark.post("/sign-in", ((request, response) -> {
+        post("/sign-in", ((request, response) -> {
             Map<String, String> model = new HashMap<String, String>();
             String username = request.queryParams("username");
             response.cookie("username", username);
