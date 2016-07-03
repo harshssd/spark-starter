01a8cdae9aa9c1370cc9e040dd103cc099028adb
Hassan Somashekara <somash@amazon.com>
2016-07-02 21:54:37 -0700

    Filter to process all the requests before the corresponding handlers.

--- src/main/java/com/hhh/spark/starter/Main.java
+++ src/main/java/com/hhh/spark/starter/Main.java
@@ -9,7 +9,9 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
+import static spark.Spark.before;
 import static spark.Spark.get;
+import static spark.Spark.halt;
 import static spark.Spark.modelAndView;
 import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
@@ -27,9 +29,24 @@ public class Main
 
         TaskDAO taskDAO = new SimpleTaskDAO();
 
+        // Filters that are processed before any requests.
+        before((request, response) -> {
+            // Using request attributes to store the values.
+            if(request.cookie("username") != null) {
+                request.attribute("username", request.cookie("username"));
+            }
+        });
+
+        before("/tasks", (request, response) -> {
+            if(request.attribute("username") == null) {
+                response.redirect("/");
+                halt();
+            }
+        });
+
         get("/", (request, response) -> {
             Map<String, String> model = new HashMap();
-            model.put("username", request.cookie("username"));
+            model.put("username", request.attribute("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -38,6 +55,7 @@ public class Main
             String username = request.queryParams("username");
             response.cookie("username", username);
             model.put("username", username);
+            response.redirect("/");
             return new ModelAndView(model, "sign-in.hbs");
         }), new HandlebarsTemplateEngine());
 
